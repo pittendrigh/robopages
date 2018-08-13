f81e26aa9dc9498fdbb472eda7ba3868f2b03335
Sandy Pittendrigh <sandy.pittendrigh@gmail.com>
2018-08-13 06:49:40 -0600

    damn

--- index.php
+++ index.php
@@ -1,31 +1,41 @@
 <?php
 
-require_once("dirCrawler.class.php");
-require_once("robopages.php");
-include_once("conf/globals.php");
+ require_once("domDrone.class.php");
+ require_once("staticDomDrone.class.php");
+ include_once("conf/globals.php");
 
 
+ global $sys_title, $sys_static_mode;
 // perhaps you want a different layout for the home page
 // ....if so edit layouts/main.xml 
-// pafasdfasf
-/*
-  if(!isset($_GET['layout']) && (!isset($_GET['page'])
-  || $_GET['page'] == "index.php"))
-  $_GET['layout'] = 'main';
- */
+// and use this
+ /*
+   if(!isset($_GET['layout']) && (!isset($_GET['robopage'])
+   || $_GET['robopage'] == "index.php"))
+   $_GET['layout'] = 'main';
+  */
 
 
-global $sys_title;
-if (isset($sys_title))
-    $title = $sys_title; //sys_title comes from conf/globals.php
-else
-    $title = "Robopages";
+ if (isset($sys_title))
+   $title = $sys_title; //sys_title comes from conf/globals.php
+ else
+   $title = "Robopages";
 
 
-$page = null;
-$page = new robopages();
+ $page = null;
+ $page = new domDrone();
 
-echo $page->startHTML($title);
-echo $page->crawl();
-echo staticRoboUtils::endHTML();
+
+ echo $page->startHTML(FALSE);
+ echo $page->printDivs();
+ echo staticRoboUtils::endHTML();
+
+
+ if (isset($sys_static_mode) && $sys_static_mode == TRUE)
+ {
+   $spage = new staticDomDrone();
+   $spage->EchoStatic($page->startHTML(TRUE), "w");
+   $spage->staticDrone();
+   $spage->EchoStatic(staticRoboUtils::endHTML(), "a");
+ }
 ?>
