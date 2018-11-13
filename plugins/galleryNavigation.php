9931a6ad04834376e473f4b51219997c01d4cbd3
Sandy Pittendrigh <sandy.pittendrigh@gmail.com>
2018-11-12 17:27:07 -0700

    robo.css in sync with recent thunbail additions to galleryNavigation.php

--- plugins/galleryNavigation.php
+++ plugins/galleryNavigation.php
@@ -8,6 +8,41 @@
  class galleryNavigation extends dynamicNavigation
  {
 
+   function hasIndexImg($link, $mode = null)
+   {
+     $ret = '';
+     $thumbName = 'tn-index.jpg';
+     // tn-index.jpg needs to be generalized to is_image in index plus suffix
+     $query = parse_url($link->href, PHP_URL_QUERY);
+     parse_str($query, $parms);
+     // we called this from linkTargetType=='dir' so we assume basename is a directory 
+     if (isset($parms['robopage']))
+     {
+       // roboresources becomes resources with next Jul 4, '14 code update
+       $base = basename($parms['robopage']);
+       $thumbUrl = $_SESSION['currentClickDirUrl'] . $base . '/roboresources/thumbs/' . $thumbName;
+       $testPath = $_SESSION['currentDirPath'] . $base . '/roboresources/thumbs/' . $thumbName;
+       if ($mode != null && $mode == 'file')
+       {
+         $thumbName = 'tn-' . staticRoboUtils::stripSuffix(basename($parms['robopage'])) . '.jpg';
+         $testPath = $_SESSION['currentDirPath'] . 'roboresources/thumbs/' . $thumbName;
+         $thumbUrl = $_SESSION['currentClickDirUrl'] . 'roboresources/thumbs/' . $thumbName;
+         //echo "new thumbName: ", $thumbName, '<br/>';
+         //echo "testPath: ", $testPath, '<br/>';
+       }
+       //echo "testPath: ", $testPath, "<br/>";
+       $test = @stat($testPath);
+       if ($test != null)
+       {
+         //echo "testPath: ", $testPath, "<br/>";
+         $ret = '<img src="' . $thumbUrl . '" alt="' . $base . '"/>';
+         //echo htmlentities($ret), "<br/>";
+       }
+     }
+
+     return $ret;
+   }
+
    function mkLink($link, $linkTargetType)
    {
      global $sys_thumb_links;
@@ -26,7 +61,14 @@
 
      if ($linkTargetType == 'dir')
      {
-         //$linklbl = '<img class="'.get_class($this) .' icon" src="' . $_SESSION["prgrmUrlRoot"] . 'systemimages/folder.png" alt="folder"/>'. "&nbsp;"  . $linklbl ;
+
+       $indexImageTest = $this->hasIndexImg($link);
+
+       if ($indexImageTest != null)
+       {
+         $linklbl = $linklbl . '<br/>' . $indexImageTest;
+       } else
+         $linklbl = '<img class="'.get_parent_class($this).' icon" src="' . $_SESSION["prgrmUrlRoot"] . 'systemimages/folder.png" alt="folder"/>' . $linklbl;
      }
 
      // grep -iH actionItem *php which of the follwing ifs?
