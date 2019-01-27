ff0f27e196ab6e9a8bce2d20b3f432f5533df14b
Sandy Pittendrigh <sandy.pittendrigh@gmail.com>
2019-01-27 15:28:34 -0700

    Rid of roboAdmin.php  leading if comment in index.php

--- index.php
+++ index.php
@@ -4,24 +4,21 @@ require_once("staticDomDrone.class.php");
 include_once("conf/globals.php");
 
 
-global $sys_title, $sys_static_mode;
-// perhaps you want a different layout for the home page
-// ....if so edit layouts/main.xml 
-// and use this
-//if (!isset($_GET['layout']) && (!isset($_GET['robopage']) || $_GET['robopage'] == "index.php"))
-//    $_GET['layout'] = 'blog';
+global $sys_static_mode;
+/*
+ perhaps you want a different layout for the home page
+ if so edit layouts/main.xml 
+ and use this
 
-
-if (isset($sys_title))
-    $title = $sys_title; //sys_title comes from conf/globals.php
-else
-    $title = "Robopages";
+if (!isset($_GET['layout']) && (!isset($_GET['robopage']) || $_GET['robopage'] == "index.php"))
+    $_GET['layout'] = 'blog';
+     or 
+    $_GET['layout'] = 'main';
+*/
 
 $page = null;
 if (isset($_SESSION['isLoggedIn']) && isset($_SESSION['privilege']) && $_SESSION['privilege'] == 'nimda')
 {
-    require_once("roboAdmin.php");
-    //$page = new roboAdmin();
     $page = new domDrone();
 }
 else
