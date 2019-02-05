1448cf5524c238c642bdd331a628422a4eebb90d
Sandy Pittendrigh <sandy.pittendrigh@gmail.com>
2019-02-05 16:41:55 -0700

    Somehow an old version of lazyLoadSlideShow.php got checked in

--- plugins/lazyLoadSlideshow.php
+++ plugins/lazyLoadSlideshow.php
@@ -1,37 +1,31 @@
 <?php
+
 @session_start();
 include_once("roboMimeTyper.php");
 include_once("conf/globals.php");
 
-class lazyLoadSlideShow extends plugin
-{
-    protected
-            $firstSlide;
-    protected
-            $slideShowPath;
-    protected
-            $slideShowUrl;
-    protected
-            $interval;
-    protected
-            $slidesNameString;
+class lazyLoadSlideShow extends plugin {
 
-    function __construct()
-    {
+    var $slideShowPath;
+    var $slideShowUrl;
+    var $interval;
+    var $slidesNameString;
+
+    function __construct() {
         
     }
 
-    function mkButtons()
-    {
+    function mkButtons() {
 
         $intervalDisplay = $this->interval / 1000;
-        $ret = "\n\n" . '
+        $ret =
+                "\n\n" . '
 <div id="Cntrl">
    <input class="booton" type="button" value="Quit" id="quit" onclick="quit()"/>
    <input class="booton" type="button" value="pause" id="stoggle" onclick="toggle()"/>
    <input class="booton" type="button" value="next" id="ntoggle" onclick="nextImage()"/>
    <input class="booton" type="button" value="prev" id="ptoggle" onclick="prevImage()"/>
-   &nbsp; &nbsp; &nbsp; ' . "\n" . '<b class="booton"> Frame Rate </b><input type="button" value="-" onclick="decInterval()"/> 
+   &nbsp; &nbsp; &nbsp; '."\n".'<b class="booton"> Frame Rate </b><input type="button" value="-" onclick="decInterval()"/> 
    <p style="display: inline;" id="incrementDisplay">' . $intervalDisplay . '</p>
    <input type="button" value="+" onclick="incInterval()"/> 
 </div>
@@ -40,38 +34,33 @@ class lazyLoadSlideShow extends plugin
         return $ret;
     }
 
-    function mkJS()
-    {
+    function mkJS() {
 
-        $ret = '
+    $ret = '
     <script type="text/javascript">
 
-       protected cnt=-1;
-       protected max=0;
+       var defaultName = \''. $_SESSION['firstSlide'] . '\';
+       var cnt=-1;
+       var max=0;
+       var thumbIDX = 0;
      
-       protected on = 1;
-       protected interval = ' . $this->interval . ';
-       protected intervalDisplay = interval/1000;
-       protected intervalHandle=null;
-       protected namesArray;
-       protected name2IDXHash  = new Object();
-       protected idx2NameHash  = new Object();
+       var on = 1;
+       var interval = ' . $this->interval . ';
+       var intervalDisplay = interval/1000;
+       var intervalHandle=null;
+       var namesArray;
+       var name2IDXHash  = new Object();
+       var idx2NameHash  = new Object();
 
        function  rollNow()
        {
-          protected str = "' . $this->slidesNameString . '";
-         
+          var str = "' . $this->slidesNameString . '";
           namesArray = str.split(\',\');
-          namesArray.sort();
           max = namesArray.length;
-          protected name;
-          
-          protected dbg=document.getElementById(\'Dbg\');
+          var name;
           for (i=0; i<namesArray.length; i++)
           {
                  name = namesArray[i]; 
-                 if(dbg != null)
-                  dbg.innerHTML=name;
                  name2IDXHash[name] = i;
                  idx2NameHash[i] = name;
           }
@@ -92,7 +81,7 @@ class lazyLoadSlideShow extends plugin
        {
            if(path == null)
               alert(\'path null\');
-           protected ret = path.split("/").reverse()[0];
+           var ret = path.split("/").reverse()[0];
            return (ret);
        }
 
@@ -100,8 +89,9 @@ class lazyLoadSlideShow extends plugin
        {
         if(on == 1)
         {
-          protected dbgObj = document.getElementById(\'Dbg\');
-          protected name = namesArray[cnt];
+          var dbgObj = document.getElementById(\'Dbg\');
+          var name = namesArray[cnt];
+          var str = \' t: \' + thumbIDX + \' c: \' + cnt ;  
           if(message == null )
               dbgObj.innerHTML = str; 
           else
@@ -111,95 +101,178 @@ class lazyLoadSlideShow extends plugin
 
        function resetMainImage(obj)
        {
-          protected mainPicObj = document.getElementById(\'currentSlideImg\');
+          var mainPicObj = document.getElementById(\'currentSlideImg\');
           mainPicObj.setAttribute("src", obj.getAttribute("src"));
-          //dbg(\'<b> \' +baseName(mainPicObj.src).replace(/.jpg|.png|.gif/i,"").replace("_"," ").replace("-"," ") + \'</b>\');
+          dbg(\'<b> \' +baseName(mainPicObj.src).replace(/.jpg|.png|.gif/i,"").replace("_"," ").replace("-"," ") + \'</b>\');
        }
 
+       function thumbShuffle()
+       {
+            // cnt has just now been changed by changeImage() slideRight() or slideLeft()
+            if(on == 1)
+            {
+               //clearInterval(intervalHandle);
+
+               var mainPicObj = document.getElementById(\'currentSlideImg\');
+               var thumbZeroObj  = document.getElementById(\'tn-0\');
+
+               var foo = cnt;
+               var imgPath = namesArray[cnt];
+               mainPicObj.setAttribute("src", imgPath); 
+               thumbZeroObj.setAttribute("value", imgPath); 
+               thumbZeroObj.setAttribute("src", imgPath); 
+               thumbZeroObj.setAttribute("title", foo % 6); 
+
+               // i=1 because we already set tn-0
+               var idx = cnt;
+               for(i=1; i<6; i++)
+               {
+                  thumbName = \'tn-\' + i;
+                  thumbObj = document.getElementById(thumbName);
+                  idx = cnt + i;
+                  if(idx >= max)
+                  {
+                    idx = 0;
+                  }
+
+                  var thumbSrc = idx2NameHash[idx];
+                  //if(thumbSrc == null)
+                  //  alert(\'null thumbSrc \' + obj.imgPath);
+                  thumbObj.setAttribute("src", thumbSrc);
+                  thumbObj.setAttribute("value", thumbSrc);
+                  thumbObj.setAttribute("title", i);
+               }
+               //intervalHandle=setInterval("advanceImage()", interval);
+             }
+       }
+
+       function slideRight()
+       {
+        if(on == 1)
+        {
+           clearInterval(intervalHandle);
+           cnt = cnt + 6;     
+           if(cnt >= max)
+           {
+              cnt = 0; 
+           }
+           thumbShuffle();
+           intervalHandle=setInterval("advanceImage()", interval);
+         } 
+       }
+
+       function slideLeft()
+       {
+        if(on == 1)
+        {
+           clearInterval(intervalHandle);
+           cnt = cnt - 6; 
+           if(cnt <= 0)
+                cnt = max - 6;
+           thumbShuffle();
+           intervalHandle=setInterval("advanceImage()", interval);
+        }
+       }
+
+       function changeImageFromThumb(obj)
+       {
+            clearInterval(intervalHandle);
+            var mainDisplayImage = document.getElementById(\'currentSlideImg\');
+            var imgPath = obj.getAttribute("value");
+            cnt = name2IDXHash[imgPath]; 
+            resetMainImage(obj); 
+
+            intervalHandle=setInterval("advanceImage()", interval);
+       }
 
        function advanceImage()
        {
            if(on == 1)
            {
-              protected picLabel;
+              var picLabel;
               cnt++;
               if(cnt >= max)
                     cnt = 0;
-              protected imgPath = idx2NameHash[cnt];
+              var imgPath = idx2NameHash[cnt];
 
-              protected imgObj = document.getElementById(\'currentSlideImg\');
-           
-              if(imgObj && imgPath)
+              var foo = cnt;
+              thumbIDX = foo % 6; 
+              var thumbName = \'tn-\' + thumbIDX;
+ 
+              var thumbObj = document.getElementById(thumbName);
+              var imgObj = document.getElementById(\'currentSlideImg\');
+              if(imgPath)
               {
-                 protected dbgObj=document.getElementById(\'Dbg\');
-                 protected tmpStr = baseName(imgPath.replace(\'.jpg\',\'\'));
-                 tmpStr = tmpStr.substring(tmpStr.lastIndexOf(\'_\')+1);
-                 dbgObj.innerHTML = \'<b>\' + tmpStr + \'</b>\';
-
+                 dbg(\'<b> \' +baseName(imgPath).replace(/.jpg|.png|.gif/i,"").replace("_"," ").replace("-"," ") + \'</b>\');
+                 //alert(imgPath);
                  imgObj.setAttribute("src", imgPath);
               }
                
+              if(thumbObj != null)
+              {
+                thumbObj.setAttribute("src",imgPath);
+                thumbObj.setAttribute("value", imgPath); 
+                thumbObj.setAttribute("title", thumbIDX);
+              }
           }
        }
 
        function nextImage()
        {
-              protected picLabel;
-
-         protected btn = document.getElementById(\'stoggle\');
-         if(on == 1)
-         {
-            on = 0;
-            btn.value=\'resume\';
-         }
-              
+              var picLabel;
               cnt++;
               if(cnt >= max)
                     cnt = 0;
-              protected imgPath = idx2NameHash[cnt];
+              var imgPath = idx2NameHash[cnt];
 
+              var foo = cnt;
+              thumbIDX = foo % 6; 
+              var thumbName = \'tn-\' + thumbIDX;
  
-              protected imgObj = document.getElementById(\'currentSlideImg\');
+              var thumbObj = document.getElementById(thumbName);
+              var imgObj = document.getElementById(\'currentSlideImg\');
+              imgObj.setAttribute("src", imgPath);
 
-              if(imgObj != null && imgPath != null && imgPath.length != 0)
-              {
-                 imgObj.setAttribute("src", imgPath);
-              }
                
-              protected dbg=document.getElementById(\'Dbg\');
-              protected tmpStr = baseName(imgPath.replace(\'.jpg\',\'\'));
-              tmpStr = tmpStr.substring(tmpStr.lastIndexOf(\'_\')+1);
-              dbg.innerHTML = \'<b>\' + tmpStr + \'</b>\';
+              if(thumbObj != null)
+              {
+                thumbObj.setAttribute("src",imgPath);
+                thumbObj.setAttribute("value", imgPath); 
+                thumbObj.setAttribute("title", thumbIDX);
+              }
+              dbg(\'<b> \' +baseName(imgPath).replace(/.jpg|.png|.gif/i,"").replace("_"," ").replace("-"," ") + \'</b>\');
        }
 
        function prevImage()
        {
 
-              protected picLabel;
+              var picLabel;
               cnt--;
               if(cnt < 0)
                     cnt = max;
-              protected imgPath = idx2NameHash[cnt];
-         protected btn = document.getElementById(\'stoggle\');
-         if(on == 1)
-         {
-            on = 0;
-            btn.value=\'resume\';
-         }
+              var imgPath = idx2NameHash[cnt];
 
+              var foo = cnt;
+              thumbIDX = foo % 6; 
+              var thumbName = \'tn-\' + thumbIDX;
  
-         protected imgObj = document.getElementById(\'currentSlideImg\');
+              var thumbObj = document.getElementById(thumbName);
+              var imgObj = document.getElementById(\'currentSlideImg\');
               imgObj.setAttribute("src", imgPath);
 
-              protected dbg=document.getElementById(\'Dbg\');
-              protected tmpStr = baseName(imgPath.replace(\'.jpg\',\'\'));
-              tmpStr = tmpStr.substring(tmpStr.lastIndexOf(\'_\')+1);
-              dbg.innerHTML = \'<b>\' + tmpStr + \'</b>\';
+               
+              if(thumbObj != null)
+              {
+                thumbObj.setAttribute("src",imgPath);
+                thumbObj.setAttribute("value", imgPath); 
+                thumbObj.setAttribute("title", thumbIDX);
+              }
+              dbg(\'<b> \' +baseName(imgPath).replace(/.jpg|.png|.gif/i,"").replace("_"," ").replace("-"," ") + \'</b>\');
        }
 
       function incInterval()
       {
-         protected iobj = document.getElementById("incrementDisplay");
+         var iobj = document.getElementById("incrementDisplay");
          interval += 2000;
          if(interval > 5000)
          {
@@ -214,7 +287,7 @@ class lazyLoadSlideShow extends plugin
 
       function decInterval()
       {
-         protected dobj = document.getElementById("incrementDisplay");
+         var dobj = document.getElementById("incrementDisplay");
          interval -= 1000;
          if(interval < 1000)
          {
@@ -228,9 +301,17 @@ class lazyLoadSlideShow extends plugin
       }
 
 
+       function stopSlideshow()
+       {
+         // not used....zap it or fix this....to not hard-coded 
+         var uri = \'?page=' . $_SESSION['currentDirUrl'] . '\';
+         window.location.replace(uri);
+       }
+
+
        function toggle()
        {
-         protected btn = document.getElementById(\'stoggle\');
+         var btn = document.getElementById(\'stoggle\');
 
          if(on == 1)
          {
@@ -241,12 +322,13 @@ class lazyLoadSlideShow extends plugin
          {
             on = 1;
             btn.value=\'pause\';
+            rollNow();
          }
        }
 
       function quit()
       {
-        location.replace("' . $_SERVER['PHP_SELF'] . "?robopage=" . $_SESSION['currentDirUrl'] . '");
+        location.replace("'.$_SERVER['PHP_SELF']."?page=".$_SESSION['currentDirUrl'].'");
       } 
 
     </script>';
@@ -254,83 +336,90 @@ class lazyLoadSlideShow extends plugin
         return $ret;
     }
 
-    function is_image($file)
-    {
+    function is_image($file) {
         $ret = FALSE;
         if (stristr($file, '.jpg') || stristr($file, '.jpeg') || stristr($file, '.gif') || stristr($file, '.png')
-        )
-        {
+        ) {
             $ret = TRUE;
         }
 
         return $ret;
     }
 
-    function getImageFilenames($dir)
-    {
+    function getImageFilenames($dir) {
 
-        $cnt = 0;
-        $ret = '';
-        $slidePaths = array();
+        $ret='';
 
-        if ($dir_handle = opendir($dir))
-        {
-            while (($file = readdir($dir_handle)) != false)
-            {
-                if ($file != '.' && $file != '..')
-                {
-                    if ($this->is_image($file))
-                    {
-                        $slidePaths[$cnt] = $file;
-                        $cnt++;
+        //echo "getImageFilenames on $dir <br/>";
+        $_SESSION['firstSlide'] = '';
+        if ($dir_handle = opendir($dir)) {
+            while (($file = readdir($dir_handle)) != false) {
+                if ($file != '.' && $file != '..') {
+                //echo $file, "<br/>"; 
+                    if ($this->is_image($file)) {
+                          $dbg = ','.  $_SESSION['currentClickDirUrl'] . 'roboresources/slideshow/' . $file;
+                          $_SESSION['firstSlide'] = $_SESSION['currentClickDirUrl'] . 'roboresources/slideshow/' . $file;
+                  //echo $_SESSION['firstSlide'], "<br/>";
+                          $ret .= $dbg;
                     }
                 }
             }
-        }
-        closedir($dir_handle);
-
-        sort($slidePaths, SORT_NATURAL | SORT_FLAG_CASE);
-        for ($i = 0; $i < $cnt; $i++)
-        {
-            $val = $slidePaths[$i];
-            $slidePaths[$i] = $_SESSION['currentClickDirUrl'] . 'roboresources/slideshow/' . $val;
-        }
-
-        $this->firstSlide = $slidePaths[0];
-        $this->slidesNameString = implode(",", $slidePaths);
-        return $this->slidesNameString;
+            closedir($dir_handle);
+        } 
+       $this->slidesNameString = $ret;
+       return $ret;
     }
 
-    function getOutput($divid = null)
-    {
+    
+
+    function getOutput($divid = null) {
         global $sys_interval;
         $ret = '';
+        $imgNames = array();
 
         if (isset($sys_interval))
             $this->interval = $sys_interval;
-        else if (!isset($this->interval))
+        else if(!isset($this->interval))
             $this->interval = 1000;
 
         $this->slideShowPath = $_SESSION['currentDirPath'] . 'roboresources/slideshow/';
-
+        //echo $this->slideShowPath , "<br/>";
         $this->slideShowUrl = $_SESSION['currentClickDirUrl'] . 'roboresources/slideshow/';
         $this->getImageFilenames($this->slideShowPath);
 
         $ret .= $this->mkJS();
+        $imgNames = explode (",", $this->slidesNameString);
+        //echo count($imgNames), "<br/>";
+        $stop = count($imgNames);
 
+        $ret .= '<div id="slideShowContainer" > ';
         $ret .= $this->mkButtons();
 
+/*
+               for($i=0; $i<6; $i++)
+               {
+                 global $imgNames; 
+                 $anImageName = $imgNames[$i];
+                 $thumbID = 'tn-' . $i; 
+                
+       $ret .= "      <p class=\"thumbContainer\"> 
+                            <img class=\"thumb\" id=\"".$thumbID."\" 
+                             title=\"$i\" 
+                             onclick=\"changeImageFromThumb(this)\" 
+                             src=\"systemimages/fuzzball.png\" alt=\"\"/> 
+                       </p>";
+               }
+*/
         $ret .= '<script type="text/javascript"> rollNow(); </script>';
-
-        $ret .= '<p id="Dbg" style="margin: 0.5em 0 0 0; padding: 0;"><b>'
-                . StaticRoboUtils::mkLabel(basename(StaticRoboUtils::stripSuffix($this->firstSlide)))
-                . '</b></p> <img id="currentSlideImg" src="' . $this->firstSlide . '" alt="' . basename($this->firstSlide)
-                . '" /> ';
-
+        //echo $_SESSION['firstSlide'], "<br/>";
+        $ret .=  '<p id="Dbg"><b>' . basename($_SESSION['firstSlide']) . '</b></p>
+                 <img id="currentSlideImg" src="'.$_SESSION['firstSlide'].'" alt="'.basename($_SESSION['firstSlide']).'" /> ';
+        $ret .=  '</div>' . "\n\n";
 
 
         return ($ret);
     }
 
 }
+
 ?>
