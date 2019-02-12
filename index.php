cccc5355ee39f44e07e9c323623ed3af0cee2477
Sandy Pittendrigh <sandy.pittendrigh@gmail.com>
2019-02-12 09:59:39 -0700

    Layouts Documentation

--- index.php
+++ index.php
@@ -3,13 +3,7 @@ require_once("domDrone.class.php");
 require_once("staticDomDrone.class.php");
 include_once("conf/globals.php");
 
-global $sys_title, $sys_static_mode;
-
-// Perhaps you want a different layout for the home page
-// If so edit layouts/main.xml 
-// and use this
-//if (!isset($_GET['layout']) && (!isset($_GET['robopage']) || $_GET['robopage'] == "index.php"))
-//    $_GET['layout'] = 'someLayout.xml';
+global $sys_static_mode;
 
 $page = new domDrone();
 
