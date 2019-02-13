2647e97da881be998df330581cd62c68760f998f
Sandy Pittendrigh <sandy.pittendrigh@gmail.com>
2019-02-13 06:21:41 -0700

    type in layouts/upload.xml, missing @stat in plugins/uploader.php, for clicking upload button before selecting a file

--- plugins/uploader.php
+++ plugins/uploader.php
@@ -19,57 +19,60 @@ class uploader extends plugin
 
         $this->action = (isset($_POST['action']) ? $_POST['action'] : (isset($_GET['action']) ? $_GET['action'] : ''));
 
-        if ($this->action == 'simple')
-        {
-
-            // ---------- SIMPLE UPLOAD ----------
-            // we create an instance of the class, giving as argument the PHP object
-            // corresponding to the file field from the form
-            // All the uploads are accessible from the PHP object $_FILES
-            $handle = new upload($_FILES['my_field']);
-
-            // then seeif the file has been uploaded properly
-            // in its *temporary* location in the server (often, it is /tmp)
-            if ($handle->uploaded)
-            {
-                // yes, the file is on the server
-                // now, we start the upload 'process'. That is, to copy the uploaded file
-                // from its temporary location to the wanted location
-                // It could be something like $handle->process('/home/www/my_uploads/');
-                $handle->process($this->dir_dest);
-
-                // we seek if everything went OK
-                if ($handle->processed)
-                {
-                    // everything was fine !
-                    $ret .= '<p class="result">';
-                    $ret .= '<a style="padding: 0.5em; display: block; width: 12em;" href="?robopage=' . $_SESSION['currentDirUrl'] . '&amp;layout=nerd"> Back to the Admin Screen</a><br/><br/>';
-                    $ret .= '  [' . $handle->file_dst_name . '] uploaded to <b>' . $_SESSION['currentDirPath'] . '</b><br />';
-                    //$ret .= '  File: <a href="' . $this->dir_pics . '/' . $handle->file_dst_name . '">' . $handle->file_dst_name . '</a>';
-                    $ret .= '   (' . round(filesize($handle->file_dst_pathname) / 256) / 4 . 'KB)';
-                    $ret .= '</p>';
-                }
-                else
-                {
-                    // one error occured
-                    $ret .= '<p class="result">';
-                    $ret .= '  <b>File not uploaded to the wanted location</b><br />';
-                    $ret .= '  Error: ' . $handle->error . '';
-                    $ret .= '</p>';
-                }
-                // we delete the temporary files
-                $handle->clean();
-            }
-            else
-            {
-                // if we're here, the upload file failed for some reasons
-                // i.e. the server didn't receive the file
-                $ret .= '<p class="result">';
-                $ret .= '  <b>File not uploaded on the server</b><br />';
-                $ret .= '  Error: ' . $handle->error . '';
-                $ret .= '</p>';
-            }
-            $ret .= "<br/><br/><hr><br/>" . $handle->log . '<br />';
+       if (isset($_FILES['my_field'])  &&  $_FILES['my_field'] != null && @stat($_FILES['my_field'] ))
+       {
+          if ($this->action == 'simple')
+          {
+  
+              // ---------- SIMPLE UPLOAD ----------
+              // we create an instance of the class, giving as argument the PHP object
+              // corresponding to the file field from the form
+              // All the uploads are accessible from the PHP object $_FILES
+              $handle = new upload($_FILES['my_field']);
+  
+              // then seeif the file has been uploaded properly
+              // in its *temporary* location in the server (often, it is /tmp)
+              if ($handle->uploaded)
+              {
+                  // yes, the file is on the server
+                  // now, we start the upload 'process'. That is, to copy the uploaded file
+                  // from its temporary location to the wanted location
+                  // It could be something like $handle->process('/home/www/my_uploads/');
+                  $handle->process($this->dir_dest);
+  
+                  // we seek if everything went OK
+                  if ($handle->processed)
+                  {
+                      // everything was fine !
+                      $ret .= '<p class="result">';
+                      $ret .= '<a style="padding: 0.5em; display: block; width: 12em;" href="?robopage=' . $_SESSION['currentDirUrl'] . '&amp;layout=nerd"> Back to the Admin Screen</a><br/><br/>';
+                      $ret .= '  [' . $handle->file_dst_name . '] uploaded to <b>' . $_SESSION['currentDirPath'] . '</b><br />';
+                      //$ret .= '  File: <a href="' . $this->dir_pics . '/' . $handle->file_dst_name . '">' . $handle->file_dst_name . '</a>';
+                      $ret .= '   (' . round(filesize($handle->file_dst_pathname) / 256) / 4 . 'KB)';
+                      $ret .= '</p>';
+                  }
+                  else
+                  {
+                      // one error occured
+                      $ret .= '<p class="result">';
+                      $ret .= '  <b>File not uploaded to the wanted location</b><br />';
+                      $ret .= '  Error: ' . $handle->error . '';
+                      $ret .= '</p>';
+                  }
+                  // we delete the temporary files
+                  $handle->clean();
+              }
+              else
+              {
+                  // if we're here, the upload file failed for some reasons
+                  // i.e. the server didn't receive the file
+                  $ret .= '<p class="result">';
+                  $ret .= '  <b>File not uploaded on the server</b><br />';
+                  $ret .= '  Error: ' . $handle->error . '';
+                  $ret .= '</p>';
+              }
+              $ret .= "<br/><br/><hr><br/>" . $handle->log . '<br />';
+           }
         }
         else
         {
