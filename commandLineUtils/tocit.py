105d584dbd151bc5badc67692a2cd8e5c958db4e
sandy <sandy.pittendrigh@gmail.com>
2020-03-14 09:54:11 -0600

    Saved a tocit.py in the wrong place two steps back. Needs to be in commandLineUtils

--- commandLineUtils/tocit.py
+++ commandLineUtils/tocit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
-### Library/Flies at beginning of each
-### first dir Library/Flies not appearing although subsequents do
-###
+
+#### buggy development code as of 3/14/2020
+### read existing file
+
 import sys
 import argparse
 import os
@@ -10,20 +11,68 @@ import mimetypes
 
 page = 1
 rootPath = os.getcwd() + "/"
-page2numHash = {}
-parser = argparse.ArgumentParser(description='uga buga')
-parser.add_argument("--prepath",default="")
+page2numDictionary = {}
+existingFileUrls = {}
+orderedPaths = []
+parser = argparse.ArgumentParser(description='Create a recursive p2n file')
+##parser.add_argument("--prepath", default="")
 args = parser.parse_args()
 
-def processP2N():
+def processexistingFileUrls():
+    #for pathUrl, myList in existingFileUrls.items():
+    #for pathUrl in existingFileUrls.keys():
+    for pathUrl in orderedPaths:
+        pathUrl = pathUrl.strip()
+        subUrl = existingFileUrls[pathUrl] 
+        for subUrl in existingFileUrls[pathUrl]:
+          displayLine = pathUrl + '/' + subUrl
+          #print(displayLine.replace('theRoot/','') )
+
+def readExistingP2NFile(filepath):
+    try:
+        fp = open(filepath)
+    except:
+        print("No fp open on p2n file")
+        exit
+    line = fp.readline()
+    cnt = 1
+    while line:
+        thisDir = ''
+        subUrl = ''
+        line = line.strip()
+        #print("line: ", line)
+        slashesCount = line.count('/')
+        if slashesCount < 1:
+            thisDir = 'theRoot'
+            subUrl = line
+        else:
+            dirs = line.split('/')
+            thisDir = dirs[0]
+            subUrl = '/'.join(dirs[1:]) 
+
+        if not thisDir in existingFileUrls:
+          orderedPaths.append(thisDir)
+
+        try:
+             existingFileUrls[thisDir].append(subUrl)
+        except:
+             existingFileUrls[thisDir] = []
+             existingFileUrls[thisDir].append(subUrl)
+
+        line = fp.readline()
+        cnt += 1
+    fp.close()
+
+
+
+def writeNewP2N():
     pageNum = 1
     f = open("p2n", "w")
-    for key in page2numHash:
-        prepath = args.prepath
-        urlpath =  prepath + key.replace(rootPath, '')
+    for key in page2numDictionary:
+        #prepath = args.prepath
+        #urlpath = prepath + key.replace(rootPath, '')
+        urlpath = key.replace(rootPath, '')
 
-        #are there an .git like files here
-        # perhaps we want to short circuit this sooner, in doDir or doFile
         skipRoboresources = re.search("^\.", urlpath)
         if skipRoboresources:
             continue
@@ -31,17 +80,22 @@ def processP2N():
         if not booleanBunt:
             continue
         skipRoboresources = re.search("roboresources", urlpath)
-        if not skipRoboresources:
-            print(urlpath)
+        if skipRoboresources:
+            continue;
+        ## if not in existing p2n data
+        ## hmm. Works sort of, but placing could improve?
+        if not urlpath in existingFileUrls:
+            print(urlpath) 
             f.write(urlpath + "\n")
-            pageNum = pageNum + 1
+        pageNum = pageNum + 1
     f.close()
 
 
-def doFile(indent, filename):
-    global page, page2numHash
 
-    page2numHash[filename] = page
+def doFile(filename):
+    global page, page2numDictionary
+
+    page2numDictionary[filename] = page
     page = page + 1
 
 
@@ -66,23 +120,19 @@ def doDir(path):
     if path[0] == '.':
         return ''
 
-    indent = ''
-    slashes = path.count('/') - 2
-    for x in range(slashes):
-        indent = indent + '\t'
     typeFs = []
     dirs = []
     for name in os.listdir(path):
-            if name[0] == '.':
-              continue
-            checkThis = os.path.join(path, name)
-            #print ("checkThis: ", checkThis)
-            if os.path.isdir(checkThis):
-                dirs.append(name)
-                #print("dir: ", checkThis)
-            else:
-                typeFs.append(name)
-                #print("typeF: ", checkThis)
+        if name[0] == '.':
+            continue
+        checkThis = os.path.join(path, name)
+        #print ("checkThis: ", checkThis)
+        if os.path.isdir(checkThis):
+            dirs.append(name)
+            #print("dir: ", checkThis)
+        else:
+            typeFs.append(name)
+            #print("typeF: ", checkThis)
 
     for file in typeFs:
         if file[0] == '.':
@@ -91,7 +141,7 @@ def doDir(path):
         skipRoboresources = re.search("roboresources", joined)
         if skipRoboresources:
             continue
-        doFile(indent, joined)
+        doFile(joined)
 
     for directory in dirs:
         if directory[0] == '.':
@@ -100,12 +150,13 @@ def doDir(path):
         skipRoboresources = re.search("roboresources", newpath)
         if skipRoboresources:
             continue
-        doFile(indent, os.path.join(path, directory))
+        doFile(os.path.join(path, directory))
 
         doDir(newpath)
 
 
-
+readExistingP2NFile(rootPath + '/p2n')
+processexistingFileUrls()
 
 doDir(rootPath)
-processP2N()
+writeNewP2N()
