5a09f28eaa5fb6e39c0e56d617d64b1e69dd2940
sandy <sandy.pittendrigh@gmail.com>
2020-03-25 13:19:27 -0600

    First working version. Still needs considerable work. For instance to rewrite as tocMaker.class.py

--- commandLineUtils/tocit.py
+++ commandLineUtils/tocit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
 
-##### buggy development code as of 3/14/2020
-##### read existing file
-##### vi terminal: this is /home/sandy/bin/tocit.py
-
 import sys
 import argparse
 import os
@@ -11,39 +7,83 @@ import re
 import mimetypes
 from pathlib import Path
 
-
 page = 1
 rootPath = os.getcwd() + "/"
 page2numDictionary = {}
-existingFileUrls = {}
+chapterUrlsDictionary = {}
 orderedPaths = []
+orderedChapters = []
 parser = argparse.ArgumentParser(description='Create a recursive p2n file')
-parser.add_argument("--prepath", default="Library/Flies/")
+parser.add_argument( "--delFromPath", default= "/var/www/html/Book/robopages/fragments/Flies/Library/Sandy-Pittendrigh/")
 args = parser.parse_args()
 
-###following is for....debugging only
-def processexistingFileUrls():
-    for pathUrl in orderedPaths:
-        pathUrl = pathUrl.strip()
-        subUrl = existingFileUrls[pathUrl] 
-        for subUrl in existingFileUrls[pathUrl]:
-          displayLine = pathUrl + '/' + subUrl
-          print(displayLine.replace('theRoot/','') )
+## ======= debugging functions
+##
+def checkOrderedChapters():
+    global orderedChapters
+    for chapter in orderedChapters:
+        print(chapter)
 
-def readExistingP2NFile(filepath):
+def checkOrderedPaths():
+    global orderedPaths
+    for path in orderedPaths:
+        print(path)
+
+def processchapterUrlsDictionary():
+    global chapterUrlsDictionary, orderedPaths
+    for pathUrl in orderedPaths:
+          pathUrl = pathUrl.strip()
+          subUrl = chapterUrlsDictionary[pathUrl]
+          for subUrl in chapterUrlsDictionary[pathUrl]:
+            displayLine = pathUrl + '/' + subUrl
+            print(displayLine.replace('theRoot/', ''))
+
+##============ end debugging functions
+
+def processPage2numDictionary(mode):
+    global page2numDictionary
+
+    print("processPage2numDictionary(" + mode + ")")
+    if mode == 'write':
+        fp = open('p2n', 'w')
+        fp.close()
+        fp = open('p2n', 'a')
+    print("len(page2numDictionary) " + str(len(page2numDictionary)))
+
+    for pathKey in page2numDictionary.keys():
+        if mode == 'print':
+            print(pathKey.replace(args.delFromPath, ''))
+        else:
+            #print ("pw: ", pathKey.replace(args.delFromPath, ''))
+            fp.write(pathKey.replace(args.delFromPath, '') + "\n")
+    fp.close()
+
+#### p2n file paths have to START at the folder where the book chapters reside
+#### so the startDirs of the paths vary
+#### /var/www/html/Book/robopages/fragments/Flies/Library/Sandy-Pittendrigh/ for instance
+#### where the book is contained in Sandy-Pittendrigh and Sandy-Pittendrigh/Dryflies is a chapter
+#### Top-level dirs in the book container dir are chapters in said book
+#### Dryflies/Mayflies/pmds.htm (all book pages are *.htm fragments,
+#### such as pmds.htm, which is *not* the case in the more general robopages context.)
+####
+def readExistingP2N(filepath):
+    global chapterUrlsDictionary, orderedPaths, orderedChapters
     try:
         Path(filepath).touch()
-        fp = open(filepath)
+        fp = open(filepath, "r")
     except:
-        print("No fp open on p2n file")
+        print("No fp to open on p2n file")
         exit
-    line = fp.readline()
+
     cnt = 1
-    while line:
+    Lines = fp.readlines()
+    for line in Lines:
         thisDir = ''
         subUrl = ''
         line = line.strip()
-        #print("line: ", line)
+        orderedPaths.append(line)
+        #print("orderedPaths append: ", line)
+
         slashesCount = line.count('/')
         if slashesCount < 1:
             thisDir = 'theRoot'
@@ -51,76 +91,111 @@ def readExistingP2NFile(filepath):
         else:
             dirs = line.split('/')
             thisDir = dirs[0]
-            subUrl = '/'.join(dirs[1:]) 
-
-        if not thisDir in existingFileUrls:
-          orderedPaths.append(thisDir)
-
+            subUrl = '/'.join(dirs[1:])
+            # we have thisDir now
+        if thisDir not in orderedChapters:
+          #print("orderedChapters.append " + thisDir)
+          orderedChapters.append(thisDir)
         try:
-             existingFileUrls[thisDir].append(subUrl)
+          chapterUrlsDictionary[thisDir].append(subUrl)
         except:
-             existingFileUrls[thisDir] = []
-             existingFileUrls[thisDir].append(subUrl)
+          chapterUrlsDictionary[thisDir] = []
+          chapterUrlsDictionary[thisDir].append(subUrl)
 
         line = fp.readline()
         cnt += 1
     fp.close()
 
+def getChapterName(thisPath):
+    slashesCount = thisPath.count('/')
+    if slashesCount < 1:
+        thisDir = 'theRoot'
+        subUrl = thisPath
+    else:
+        dirs = thisPath.split('/')
+        thisDir = dirs[0]
+        ##subUrl = '/'.join(dirs[1:])
+    return (thisDir)
 
-
+## orderedPaths is a list which is a copy of any existing p2n file
+## From orderedPaths get a "thisDir" (perhaps rename as thisChapter)
+## by peeling off the most left-side path directory
+## Now print as is from orderedPaths until a new thisDir is encountered
+## Now work with chapterUrlsDictionary to find new urls not already in old p2n.
+## if so write them (append to this chapter) now.
+## Now move onto the next thisDir
+## ...will we ever need page2numDictionary? Ah useful for "already got this url"
+## chapterUrlsDictionary has chapter dirNames as keys holding a list of subUrls
+## For instance "Dryflies" might be a key with 'Mayflies/Ducktails/Ducktail-pmds.htm'
+## as one of many subpaths
+##
 def writeNewP2N():
+    global page2numDictionary, chapterUrlsDictionary, orderedPaths
+
+    length = len(orderedPaths)
+    print("len(orderedPaths) " + str(length))
     pageNum = 1
-    f = open("p2n", "w")
-    for key in page2numDictionary:
-        prepath = args.prepath
-        urlpath = prepath + key.replace(rootPath, '')
+    if length > 0:
+        f = open("np2n", "w")
+        thisPath = orderedPaths[pageNum]
+        thisDir = getChapterName(thisPath)
+        lastDir = thisDir
+        while thisDir == lastDir and pageNum < length:
+            thisPath = orderedPaths[pageNum]
+            thisDir = getChapterName(thisPath)
+            if thisDir == lastDir:
+                print(orderedPaths[pageNum])
+            else:
+                print("thisDir: " + thisDir)
+                if chapterUrlsDictionary[thisDir] != None:
+                  for subUrl in chapterUrlsDictionary[thisDir]:
+                    if subUrl not in page2numDictionary:
+                      ##write????????
+                      print(subUrl)
+            lastDir = thisDir
+            pageNum += 1
+        f.close()
+    else:
+        print ("call processPage2numDictionary")
+        processPage2numDictionary('write')
 
-        skipRoboresources = re.search("^\.", urlpath)
-        if skipRoboresources:
-            continue
-        booleanBunt = tocMimer(key)
-        if not booleanBunt:
-            continue
-        skipRoboresources = re.search("roboresources", urlpath)
-        if skipRoboresources:
-            continue;
-        ## if not in existing p2n data
-        ## hmm. Works sort of, but placing could improve?
-        if not urlpath in existingFileUrls:
-            print(urlpath) 
-            f.write(urlpath + "\n")
-        pageNum = pageNum + 1
-    f.close()
-
-
-
+## ##f.write(urlpath + "\n")
+## not doing tocMimer here? Allow isdir and *.htm only
+## for now we are relying on good input from afar
+##
 def doFile(filename):
-    global page, page2numDictionary
-
-    page2numDictionary[filename] = page
-    page = page + 1
-
+    global page 
+    
+    if tocMimer(filename):
+      print ("doFile: ", filename)
+      page2numDictionary[filename] = page
+      page = page + 1
 
 def tocMimer(path):
     ret = False
     if os.path.isdir(path) == True:
-        ##deal with roboresources???
         ret = True
 
-    types = [
-        ".jpg", ".JPG", ".JPEG", ".png", ".gif", ".htm", ".html", ".tgz",
-        ".zip", ".pdf", ".smil", ".xml", "xhtml"
-    ]
+    ## zips pdf and everything else must be wrapped
+    ## in an *.htm fragment.  This is a book not a website
+    ## ...for now anyway
+    types = [".htm"]
     filename, suffix = os.path.splitext(path)
     if suffix in types:
         ret = True
 
     return (ret)
 
-
-def doDir(path):
+## recurseDirs makes no assignments
+## but it does twice in its codes) call doFile(somePath)
+## doFile increments the global page number and
+## also inserts to the global page2numDictionary
+## Paths start absolute but get stored from the chapter
+## leve down. (path.replace(args.delFromPath,'')
+##
+def recurseDirs(path):
     if path[0] == '.':
-        return ''
+        return ('')
 
     typeFs = []
     dirs = []
@@ -128,7 +203,6 @@ def doDir(path):
         if name[0] == '.':
             continue
         checkThis = os.path.join(path, name)
-        #print ("checkThis: ", checkThis)
         if os.path.isdir(checkThis):
             dirs.append(name)
             #print("dir: ", checkThis)
@@ -139,7 +213,7 @@ def doDir(path):
     for file in typeFs:
         if file[0] == '.':
             continue
-        joined = os.path.join(path, file)
+        joined = os.path.join(path, file).replace(args.delFromPath, '')
         skipRoboresources = re.search("roboresources", joined)
         if skipRoboresources:
             continue
@@ -148,17 +222,18 @@ def doDir(path):
     for directory in dirs:
         if directory[0] == '.':
             continue
-        newpath = os.path.join(path, directory)
+        newpath = os.path.join(path, directory).replace(args.delFromPath, '')
         skipRoboresources = re.search("roboresources", newpath)
         if skipRoboresources:
             continue
         doFile(os.path.join(path, directory))
 
-        doDir(newpath)
+        recurseDirs(newpath)
 
+readExistingP2N(rootPath + '/p2n')
+#checkOrderedPaths()
+#checkOrderedChapters()
 
-readExistingP2NFile(rootPath + '/p2n')
-##processexistingFileUrls()
-
-doDir(rootPath)
+recurseDirs(rootPath)
+#processPage2numDictionary("print") ## print mode means debugging
 writeNewP2N()
