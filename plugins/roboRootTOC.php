cc2db00632303d7061dcb521a17409518f3111f1
pittendrigh <sandy.pittendrigh@gmail.com>
2022-03-13 16:24:00 -0500

    Bug fixed in roboRootTOC.php on layout=main buttonState

--- plugins/roboRootTOC.php
+++ plugins/roboRootTOC.php
@@ -8,111 +8,154 @@ include_once("dynamicNavigation.php");
 class roboRootTOC  extends dynamicNavigation
 {
 
- function init() {
- $this->linkshash = null;
- $this->linkshash = array();
+   function init()
+    {
+        $this->linkshash = null;
+        $this->linkshash = array();
 
- $this->fileKeys = null;
- $this->fileKeys = array();
+        $this->fileKeys = null;
+        $this->fileKeys = array();
 
- $this->imageKeys = null;
- $this->imageKeys = array();
+        $this->imageKeys = null;
+        $this->imageKeys = array();
 
- $this->dirKeys = null
- $this->dirKeys = array();
+        $this->dirKeys = null;
+        $this->dirKeys = array();
 
- $this->mimer = new roboMimeTyper();
+        $this->mimer = new roboMimeTyper();
 
- $this->currentDirPath = $_SESSION['prgrmDocRoot'];
- $this->currentDirUrl = ''; 
- $this->currentClickDirUrl = 'fragments/';
+        $this->currentDirPath = $_SESSION['prgrmDocRoot'];
+        $this->currentDirUrl = ''; 
+        $this->currentClickDirUrl = 'fragments/';
 
- $this->gatherLinks();
- }
+        $this->gatherLinks();
+    }
 
- function lookWhereForFiles()
- {
- $handle = @opendir($_SESSION['prgrmDocRoot']);
- return ($handle);
- }
+    function lookWhereForFiles()
+    {
+        $handle = @opendir($_SESSION['prgrmDocRoot']);
+        return ($handle);
+    }
 
 
 function getTOCJs()
 {
- $ret = '';
- $ret .= <<<ENDO
- <script>
- function flipAndRedraw() {
-  var x = document.getElementById("tocComesAndGoes");
-  var b = document.getElementById("tocPopper");
-  if (x.style.display === "none") {
-   x.style.display = "block";
-   b.innerHTML="toc";
-  } else {
-   x.style.display = "none";
-   b.innerHTML="TOC";
-  }
+$ret = '';
+$ret .= <<<ENDO
+<script>
+function flipAndRedraw()
+{
+var x = document.getElementById("tocComesAndGoes");
+var b = document.getElementById("tocPopper");
+if (x.style.display === "none")
+{
+x.style.display = "block";
+b.innerHTML="toc";
+}
+else
+{
+x.style.display = "none";
+b.innerHTML="TOC";
+}
 }
 </script>
 ENDO;
 
-  return $ret;
- }
+return $ret;
+}
+    function getOutput($divid)
+    {
+        global $sys_show_suffixes, $sys_thumb_links;
 
- function getOutput($divid)
- {
- global $sys_show_suffixes, $sys_thumb_links;
+        $indexFlag = FALSE;
+        $slideshowFlag = FALSE;
+        $ret=$indexHref = '';
 
- $indexFlag = FALSE;
- $slideshowFlag = FALSE;
- $ret=$indexHref = '';
-
- $state = 'toc';
- if (isset($_COOKIE['buttonState']) 
- && in_array($_COOKIE['buttonState'], ['toc', 'TOC']))
- {
- //if($_SESSION['layout'] == 'robo'){
- $state = $_COOKIE['buttonState'];
- //} 
- }
+      $state = 'toc';
+      if (isset($_COOKIE['buttonState']) 
+          && in_array($_COOKIE['buttonState'], ['toc', 'TOC']))
+      {
+        //if($_SESSION['layout'] == 'robo'){
+           $state = $_COOKIE['buttonState'];
+        //} 
+      }
 
 
  if($_SESSION['layout'] != 'main'){
- $ret .= '<button id="tocPopper" onClick="flipAndRedraw()">'.$state.'</button>';
- $ret .= $this->getTOCJs();
+        $ret .= '<button id="tocPopper" onClick="flipAndRedraw()">'.$state.'</button>';
+        $ret .= $this->getTOCJs();
+ } else {
+  $state = 'toc';
  }
- $ret .= '<div id="tocComesAndGoes">';
+        $ret .= '<div id="tocComesAndGoes">';
 
- $cnt = count($this->linkshash);
+        $cnt = count($this->linkshash);
 
- if (!$slideshowFlag && @stat($this->currentDirPath . 'roboresources/slideshow'))
- {
- $slideshowFlag = TRUE;
- if( $slideshowFlag )
- $ret .= $this->getSlideshowLink();
- }
-
-
- // fileKeys was made in the ctor
- $dcnt = count($this->dirKeys);
- $icnt = count($this->imageKeys);
- $fcnt = count($this->fileKeys);
+        if (!$slideshowFlag && @stat($this->currentDirPath . 'roboresources/slideshow'))
+        {
+            $slideshowFlag = TRUE;
+            if( $slideshowFlag )
+              $ret .= $this->getSlideshowLink();
+        }
 
 
- $allOfEm = array_merge($this->dirKeys,$this->fileKeys,$this->imageKeys);
- foreach($allOfEm as $aKey) {
-  $link = $this->linkshash[$aKey];
-  $ret .= "\n" . $this->mkLink($link) . "\n";
- } 
+        // fileKeys was made in the ctor
+        $dcnt = count($this->dirKeys);
+        $icnt = count($this->imageKeys);
+        $fcnt = count($this->fileKeys);
 
- // any index link like ?robopage=index.htm or index.jpg made to come last
- if ($indexFlag) {
-  $ret .= "\n" . $this->mkLink($indexLink) . "\n";
- }
+/*
+        for ($i = 0; $i < $dcnt; $i++)
+        {
+            $akey = $this->dirKeys[$i];
+            $link = $this->linkshash[$akey];
+            if ($link != null && !strstr($link->href, "slideshow"))
+            {
+                $ret .= "\n" . $this->mkLink($link) . "\n";
+            }
+        }
+
+        for ($i = 0; $i < $icnt; $i++)
+        {
+            $akey = $this->imageKeys[$i];
+            $link = $this->linkshash[$akey];
+            if ($link != null && !strstr($link->href, "slideshow"))
+            {
+                $ret .= "\n" . $this->mkLink($link) . "\n";
+            }
+        }
+
+        for ($i = 0; $i < $fcnt; $i++)
+        {
+            $akey = $this->fileKeys[$i];
+            $link = $this->linkshash[$akey];
+            if ($link != null && !strstr($link->href, "slideshow"))
+            {
+                $ret .= "\n" . $this->mkLink($link) . "\n";
+            }
+        }
+
+*/
+
+
+$allOfEm = array_merge($this->dirKeys,$this->fileKeys,$this->imageKeys);
+foreach($allOfEm as $aKey)
+{
+                //echo $aKey, "<br/>";
+                $link = $this->linkshash[$aKey];
+                $ret .= "\n" . $this->mkLink($link) . "\n";
+} 
+
+
+        // any index link like ?robopage=index.htm or index.jpg made to come last
+        if ($indexFlag)
+        {
+            $ret .= "\n" . $this->mkLink($indexLink) . "\n";
+        }
  
- $ret .= '</div>';
+        $ret .= '</div>';
 
- $ret .= '<script> fixItUp("' . $state . '");</script>';
- return $ret;
- }
+        $ret .= '<script> fixItUp("' . $state . '");</script>';
+        return $ret;
+    }
 }
